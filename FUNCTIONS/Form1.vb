360740efe11d479ed35eb8ca78417a64e638728f
Annah61 <annahmoore.morerwa@gmail.com>
2023-09-09 22:36:12 +0200

    Add project files.

---
+++ FUNCTIONS/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub btnRunfunction_Click(sender As Object, e As EventArgs) Handles btnRunfunction.Click
+        Dim answer As Double = solveMath()
+        MessageBox.Show(answer)
+    End Sub
+
+
+    Private Function solveMath() As Double
+        Dim num1 As Integer = 30
+        Dim num2 As Integer = 5
+        Return num1 / num2
+
+    End Function
+End Class
